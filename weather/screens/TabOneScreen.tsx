12266f99da91ac881c4c0ced9307f0bda1085853
pedernin <pedernin@students.zhaw.ch>
2022-04-28 13:56:33 +0200

    update

--- weather/screens/TabOneScreen.tsx
+++ weather/screens/TabOneScreen.tsx
@@ -3,33 +3,47 @@ import { StyleSheet, TextInput } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import axios from 'axios';
+import { weatherConditions } from '../constants/WeatherConditions';
 
 export interface Forecast {
   main: String;
   location: String;
+  country: String;
   temp: number;
+}
+
+export interface Lonlat {
+  lon: number;
+  lat: number;
+}
+
+const initLonLat: Lonlat = {
+  lon: 0,
+  lat: 0,
 }
 
 const initForecast: Forecast ={
   main: "",
   location: "",
+  country: "",
   temp: 0,
 }
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-const [text, onChangeText] = useState('London');
+const [text, onChangeText] = useState('');
 const [forecast, setForecast] = useState<Forecast>(initForecast)
-const [lat, setLat] = useState<number>(0);
-const [lon, setLon] = useState<number>(0);
+const [lonlat, setLonLat] = useState<Lonlat>(initLonLat);
 
 const latlonUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${text}&limit=1&appid=82899956a4b3c21ea1f16a30c3eed6c0`
-const cityUrl = `https://api.openweathermap.org/data/2.5/weather?&lat=${lat}&lon=${lon}&appid=82899956a4b3c21ea1f16a30c3eed6c0`
+const cityUrl = `https://api.openweathermap.org/data/2.5/weather?&lat=${lonlat.lat}&lon=${lonlat.lon}&appid=82899956a4b3c21ea1f16a30c3eed6c0`
 useEffect(() => {  
   axios.get(latlonUrl)
       .then(res => {
-        setLat(res.data[0].lat)
-        setLon(res.data[0].lon)
+        setLonLat({lon: res.data[0].lon, lat: res.data[0].lat})
         forecast.location = res.data[0].name
+        forecast.country = res.data[0].country
+      }).catch(e => {
+        console.log(e);
       })
   axios.get(cityUrl)
   .then(res => {
@@ -37,12 +51,14 @@ useEffect(() => {
       forecast.temp = Math.round(res.data.main.temp - 272)
       //console.log(res.data.weather[0].main);
       
+  }).catch(e => {
+    console.log(e);
   })
 }, [latlonUrl, cityUrl])
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Weather App</Text>
       <TextInput style={styles.input} onChangeText={onChangeText} value={text} placeholder={" Enter Location..."} />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Text style={styles.subtitle}>Weather Description</Text>
@@ -50,7 +66,7 @@ useEffect(() => {
       <Text style={styles.subtitle}>Temperature</Text>
       <Text>{forecast.temp}°C</Text>
       <Text style={styles.subtitle}>Location</Text>
-      <Text>{forecast.location}</Text>
+      <Text>{forecast.location + ", " + forecast.country}</Text>
     </View>
   );
 }
